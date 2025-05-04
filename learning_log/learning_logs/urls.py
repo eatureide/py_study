aa6dfeb70edf64e4d033bef9d465986c877d4604
Eature <781746820@qq.com>
2025-05-04 17:37:30 +0800

    account

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # 主页
     path('', views.index, name='index'),
     # 显示所有主题
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    # 特定主题的详情页面
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -11,5 +11,11 @@ def index(request):
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
-    print(topics)
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
